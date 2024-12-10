30b9bf086ebd4973e60408a0e025621208da3f21
Daniil <danicar.game@gmail.com>
2024-12-10 19:33:56 +0300

    A lot of changes

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -1,7 +1,22 @@
-import unittest
+import os
 from visualizer import visualize_with_plantuml
 
-class TestVisualizer(unittest.TestCase):
-    def test_visualize_with_plantuml(self):
-        # Тестировать вызов subprocess можно, подменив `visualize_with_plantuml` на мок.
-        pass  # Заглушка для моков.
+def test_visualize_with_plantuml():
+    # Подготовка
+    plantuml_path = "C:/path/to/plantuml.jar"  # Убедитесь, что путь к JAR-файлу PlantUML верный
+    uml_file = "test_output.uml"
+    png_file = "test_output.png"
+
+    # Создаем тестовый UML-файл
+    with open(uml_file, "w") as f:
+        f.write("@startuml\nrectangle Test\n@enduml")
+
+    # Генерация PNG
+    visualize_with_plantuml(plantuml_path, uml_file, png_file)
+
+    # Проверяем, что PNG-файл создан
+    assert os.path.exists(png_file)
+
+    # Удаляем временные файлы
+    os.remove(uml_file)
+    os.remove(png_file)
